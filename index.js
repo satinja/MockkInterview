20333fa647013beaf14ed07df46d30d862f44a28
Kanishk Yadav <yadavkanishk57@gmail.com>
2023-01-08 17:26:37 +0530

    Added start_apti api and connected with database

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express'); //Import the express dependency
 var mongoose = require('mongoose');
-
+console.log("Server Side code is running");
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
 app.use(express.static(__dirname + '/public'));
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
+app.use(express.static('public'));
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
@@ -20,6 +21,7 @@ app.post('/login',function(req,res){
 		db.collection('users').findOne(query, function(err, result){
 			if(err) throw err;
 			client.close();
+			console.log(result);
 			if(result.password == req.body.password)
 			{
 				console.log("You are logged in!");
@@ -63,14 +65,37 @@ app.get('/reset_password',function(req,res){
 app.get('/signup1',function(req,res){
    res.sendFile('signup.html', {root: __dirname}); 
 });
-
-app.post('/check_your_email',function(req,res){
+//chnaged post to get
+app.get('/check_your_email',function(req,res){
    res.sendFile('check_your_email.html', {root: __dirname}); 
 });
+
+app.get('/test',function(req,res){
+	res.sendFile('test.html', {root: __dirname}); 
+ });
 
 app.post('/account_password',function(req,res){
    res.sendFile('account_password.html', {root: __dirname}); 
 });
+//for the server side of the button code
+
+
+app.post('/start_apti',function(req, res){
+	var MongoClient = require('mongodb').MongoClient;
+	 MongoClient.connect('mongodb://admin:password@localhost:27017', function(err, client){
+		 if(err) throw err;
+		 console.log("DB Connected for Test Button!");
+		 var db = client.db('user_db');
+		 var query = {question: '1'};
+		console.log();
+		 db.collection('Apti_Questions').findOne(query, function(err, result){
+			 if(err) throw err;
+			 client.close();
+			 console.log(result);
+		 });
+	 });
+ });
+ 
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
